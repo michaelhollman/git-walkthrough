19d709371fcc2ff92dc049439c7cc628ea085236
Michael Hollman <hollyfrass@gmail.com>
2016-02-17 19:07:08 -0600

    adding documentation

--- csvstring/CSVString.java
+++ csvstring/CSVString.java
@@ -1,13 +1,37 @@
+/**
+ * Holds a set of information using Strings (and only strings). Functions
+ * similarly to a primative ArrayList or stack. Holds all information in one
+ * massive String, separating each element with a delimiter (by default a comma,
+ * hence CSV).
+ * 
+ * CSVStrings are zero indexed!!!
+ * 
+ * @author Michael Hollman
+ * @version 0.6
+ * 
+ */
 public class CSVString {
 
   private String delim;;
 	private String str;
 
+	/**
+	 * Creates an empty CSVString.
+	 */
 	public CSVString() {
 		delim = ",";
 		str = "";
 	}
 
+	/**
+	 * Constructs an empty CSVString with {@code x} number of empty elements.
+	 * Calling {@link #addSingleItem(String)} or {@link #addManually(String)}
+	 * will append to the end, leaving {@code x} number of empty elements
+	 * untouched.
+	 * 
+	 * @param x
+	 *            number of empty elements in the CSV.
+	 */
 	public CSVString(int x) {
 		this();
 		for (int i = 0; i < x - 1; i++) {
@@ -15,15 +39,37 @@ public class CSVString {
 		}
 	}
 
+	/**
+	 * Constructs a CSVString with the provided string. Values must already be
+	 * separated by the default delimiter. Use caution when using this
+	 * constructor.
+	 * 
+	 * @param str
+	 *            a "pre-formatted" CSVString, with values already separated by
+	 *            the delimiter.
+	 */
 	public CSVString(String str) {
 		this();
 		this.str = str;
 	}
 
+	/**
+	 * Use a custom string for the delimiter. Use with extreme caution.
+	 * 
+	 * @param delim
+	 *            custom delimiter.
+	 */
 	protected void useCustomDelim(String delim) {
 		this.delim = delim;
 	}
 
+	/**
+	 * Add another element to the CSV. Note: Do not include any delimiting
+	 * characters in the parameter.
+	 * 
+	 * @param str
+	 *            Single element to add to the end of the CSV.
+	 */
 	public void addSingleItem(String str) {
 		if (this.size() == 0) {
 			this.str = str;
@@ -32,6 +78,11 @@ public class CSVString {
 		}
 	}
 
+	/**
+	 * Counts the number of elements in the CSVString. Similar to Array.length.
+	 * 
+	 * @return the number of elements in the CSVString.
+	 */
 	public int size() {
 		String partialStr = str;
 		if (partialStr.indexOf(delim) != -1) {
@@ -51,6 +102,16 @@ public class CSVString {
 		}
 	}
 
+	/**
+	 * Returns the String value of the String element at the given index (zero
+	 * indexing).
+	 * 
+	 * @param x
+	 *            The index of the String to return.
+	 * @return the String value at index x.
+	 * @throws IndexOutOfBoundsException
+	 *             if x < 0 or x >= {@link #size()}.
+	 */
 	public String index(int x) {
 		String partialStr = str;
 		if (x >= 0 && x < size()) { // within bounds
@@ -73,6 +134,17 @@ public class CSVString {
 		}
 	}
 
+	/**
+	 * Changes the value of the String element and the given index (zero
+	 * indexing).
+	 * 
+	 * @param x
+	 *            Index of the element to change.
+	 * @param newValue
+	 *            new String value of element.
+	 * @throws IndexOutOfBoundsException
+	 *             if x < 0 or x >= {@link #size()}.
+	 */
 	public void setIndex(int x, String newValue) {
 		String front = "";
 		String end = "";
@@ -89,10 +161,22 @@ public class CSVString {
 		}
 	}
 
+	/**
+	 * @return true if, and only if, {@link #size()} is 0.
+	 */
 	public boolean isEmpty() {
 		return str.isEmpty();
 	}
 
+	/**
+	 * Compares given string to the elements of the CSVString. Uses
+	 * {@link String#equals(Object)}.
+	 * 
+	 * @param s
+	 *            The String element to find in the CSVString.
+	 * @return true if the give string exactly matches any of the elements of
+	 *         the CSVString.
+	 */
 	public boolean contains(String s) {
 		boolean found = false;
 		for (int i = 0; i < size() && !found; i++) {
@@ -101,6 +185,13 @@ public class CSVString {
 		return found;
 	}
 
+	/**
+	 * Searches the CSVString for the exact string provided. Returns it's index.
+	 * 
+	 * @param s
+	 *            String to search for.
+	 * @return the FIRST index of the given string, or -1 if it cannot be found.
+	 */
 	public int indexOf(String s) {
 		int index = -1;
 		if (this.contains(s)) {
@@ -111,10 +202,16 @@ public class CSVString {
 					found = true;
 				}
 			}
-		}
+		} // didn't contain s, index still = -1
 		return index;
 	}
 
+	/**
+	 * @return the exact string that contains all the information for the
+	 *         CSVString, including delimiters.
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return str;
--- csvstring/CSVStringMatrix.java
+++ csvstring/CSVStringMatrix.java
@@ -1,16 +1,34 @@
+/**
+ * A "2-dimensional" CSVString. Achieved using a CSVString containing string
+ * representations of CSVStrings. Uses a semi-colon as default delimiter.
+ * Zero-indexed.
+ * 
+ * When adding CSVStrings to a CSVStringMatrix, use
+ * CSVStringMatrix.addSingleItem(CSVString.toString()).
+ * 
+ * @author Michael Hollman
+ * @version 0.1
+ * @see CSVString
+ */
 public class CSVStringMatrix extends CSVString {
 
+  /**
+	 * Creates an empty CSVString matrix.
+	 */
 	public CSVStringMatrix() {
 		super();
-        
-        
-		this.useCustomDelim(";" ) ;
+		this.useCustomDelim(";");
 	}
 
-	public CSVString getSubCSV(int i) 
-    {
-		
-        return new CSVString(this.index(i));
+	/**
+	 * @param index
+	 *            index of the CSVString to get.
+	 * @return the CSVString found at the given index.
+	 * @throws IndexOutOfBoundsException
+	 *             if i < 0 or i > size().
+	 */
+	public CSVString getSubCSV(int i) {
+		return new CSVString(this.index(i));
 	}
 
 }
